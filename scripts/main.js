8056f44c130216902a384e108b6248b55188ae9f
Andrii Kotliar <andrikotliar@gmail.com>
2021-01-11 20:29:08 +0200

    sw.js init

--- scripts/main.js
+++ scripts/main.js
@@ -312,3 +312,13 @@ addAward.addEventListener('click', () => {
     addLine(awards, 'award-title', 'award-nominations');
 });
 modalOverlay.addEventListener('click', closeModal);
+
+// register service worker
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(() => navigator.serviceWorker.ready.then((worker) => {
+      worker.sync.register('syncdata');
+    }))
+    .catch((err) => console.log(err));
+}
